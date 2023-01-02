de2de8edda29d932fd1ce6713bfa7b4cd26527db
Alexander Entinger <consulting@lxrobotics.com>
2023-01-02 08:52:24 +0100

    Fix: AudioClass does not have member 'close', use 'end' instead.

--- examples/SimpleAudioPlayerZero/SimpleAudioPlayerZero.ino
+++ examples/SimpleAudioPlayerZero/SimpleAudioPlayerZero.ino
@@ -56,7 +56,7 @@ void loop()
   
   // until the file is not finished  
   AudioZero.play(myFile);
-  AudioZero.close();
+  AudioZero.end();
   Serial.println("End of file. Thank you for listening!");
   while (true) ;
 }
